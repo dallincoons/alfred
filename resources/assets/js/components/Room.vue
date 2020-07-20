b326c948faa53c5cf3aa5c3cf1cc250cdae0fff6
Dallin <dallinis@hotmail.com>
2020-07-20 17:09:03 -0600

    Adds queue functionality and styling

--- resources/assets/js/components/Room.vue
+++ resources/assets/js/components/Room.vue
@@ -63,23 +63,36 @@
             <div class="buffer" v-show="playlistVisible"></div>
             <div class="playlist-wrapper" v-show="playlistVisible">
                 <div class="add-or-queue-wrapper" v-show="playlistVisible">
-                    <div class="add-song selected">Added</div>
-                    <div class="que-song">Queue Up</div>
+                    <div class="add-song" :class="{selected : !queueUpVisible}" @click="queueUpVisible = false">Added</div>
+                    <div class="que-song" :class="{selected : queueUpVisible}" @click="queueUpVisible = !queueUpVisible">Queue Up</div>
                 </div>
                 <div class="playlist-songs-section" v-show="!songSearched">
                     <div v-for="song in room_songs" class="playlist-song-wrapper">
                         <div class="playlist-song-info">
                             <span class="song-title" @click="playSelectedSong(song.id)">{{ song.title }}</span>
-                            <span class="song-artist">{{song.artist_title}}</span>
-                            <h6>Added by {{song.added_by}}</h6>
+                            <span class="song-artist">{{song.artist_title}} <span> | {{song.added_by}}</span></span>
                         </div>
-                        <span class="playlist-song-delete" @click="deleteSong(song)">
+                        <span class="playlist-song-delete" @click="deleteSong(song)" v-show="!queueUpVisible">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 135.06 135.06">
                                 <rect class="cls-remove" x="30.03" y="54.53" width="75" height="26" rx="3"/>
                                 <circle class="cls-remove-circle" cx="67.53" cy="67.53" r="64.53"/>
                             </svg>
                         </span>
-                        <span @click="queueSong(song.external_id)"></span>
+                        <span @click="queueSong(song.external_id)" v-show="queueUpVisible" class="playlist-song-delete">
+                            <span v-show="!queued">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 135.06 135.06">
+                                <path class="cls-que" d="M66.29,42c-1.43-.83-2.6-.15-2.6,1.5V50.3a5.77,5.77,0,0,0,2.6,4.5l20.7,12c1.43.82,1.43,2.17,0,3l-20.7,12a5.76,5.76,0,0,0-2.6,4.5v4.14c0,1.65,1.17,2.32,2.6,1.5l40.55-23.41c1.43-.83,1.43-2.18,0-3Z"/>
+                                <path class="cls-que" d="M28.23,43.22c-1.43-.82-2.6-.15-2.6,1.5V51.5a5.76,5.76,0,0,0,2.6,4.5l20.7,12c1.43.82,1.43,2.17,0,3l-20.7,12a5.76,5.76,0,0,0-2.6,4.5v4.14c0,1.65,1.17,2.32,2.6,1.5L68.78,69.64c1.43-.83,1.43-2.18,0-3Z"/>
+                                <circle class="cls-que-circle" cx="67.53" cy="67.53" r="64.53"/>
+                            </svg>
+                        </span>
+                        <span v-show="queued">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 135.06 135.06">
+                                <path class="cls-check" d="M106.76,49.25A3,3,0,0,0,107,45l-6-6.66a3,3,0,0,0-4.23-.22l-42.06,38A3.17,3.17,0,0,1,50.33,76L38.86,64.35a3,3,0,0,0-4.24,0l-6.4,6.3a3,3,0,0,0,0,4.25l18.62,18.9C48,95,48.93,95.9,48.94,95.89s.65.67,1.41,1.51,2.4.64,3.62-.47Z"/>
+                                <circle class="cls-check-circle" cx="67.53" cy="67.53" r="64.53"/>
+                            </svg>
+                        </span>
+                        </span>
                     </div>
                 </div>
                 <spotify-web-player
@@ -110,8 +123,8 @@
                 </div>
             </div>
             <div class="add-or-queue-wrapper" v-show="songSearched">
-                <div class="add-song selected">Add</div>
-                <div class="que-song">Queue Up</div>
+                <div class="add-song" :class="{selected : !queueUpVisible}" @click="queueUpVisible = false">Add</div>
+                <div class="que-song" :class="{selected : queueUpVisible}" @click="queueUpVisible = !queueUpVisible">Queue Up</div>
             </div>
             <div class="searched-songs-wrapper" v-show="songSearched">
                 <div v-for="item in songs"  class="song-wrapper">
@@ -119,7 +132,7 @@
                         <span class="song-title">{{item.name}}</span>
                         <div class="song-artist">{{ item.album.artists[0].name }} <span class="song-added-by"> | {{item.added_by}}</span></div>
                     </div>
-                    <div @click="addSong(rkey, item)" class="song-item search-song">
+                    <div @click="addSong(rkey, item)" class="song-item search-song" v-show="!queueUpVisible">
                         <div class="add-remove-songs-wrapper">
                             <div v-if="songIsNotAdded(item)" class="add-song-wrapper">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 135.06 135.06">
@@ -135,6 +148,24 @@
                             </div>
                         </div>
                     </div>
+                    <div @click="queueSong(item.id)" v-show="queueUpVisible" class="search-song-queue song-item">
+                        <span v-show="!queued">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 135.06 135.06">
+                                <path class="cls-que" d="M66.29,42c-1.43-.83-2.6-.15-2.6,1.5V50.3a5.77,5.77,0,0,0,2.6,4.5l20.7,12c1.43.82,1.43,2.17,0,3l-20.7,12a5.76,5.76,0,0,0-2.6,4.5v4.14c0,1.65,1.17,2.32,2.6,1.5l40.55-23.41c1.43-.83,1.43-2.18,0-3Z"/>
+                                <path class="cls-que" d="M28.23,43.22c-1.43-.82-2.6-.15-2.6,1.5V51.5a5.76,5.76,0,0,0,2.6,4.5l20.7,12c1.43.82,1.43,2.17,0,3l-20.7,12a5.76,5.76,0,0,0-2.6,4.5v4.14c0,1.65,1.17,2.32,2.6,1.5L68.78,69.64c1.43-.83,1.43-2.18,0-3Z"/>
+                                <circle class="cls-que-circle" cx="67.53" cy="67.53" r="64.53"/>
+                            </svg>
+                        </span>
+                        <span v-show="queued">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 135.06 135.06">
+                                <path class="cls-check" d="M106.76,49.25A3,3,0,0,0,107,45l-6-6.66a3,3,0,0,0-4.23-.22l-42.06,38A3.17,3.17,0,0,1,50.33,76L38.86,64.35a3,3,0,0,0-4.24,0l-6.4,6.3a3,3,0,0,0,0,4.25l18.62,18.9C48,95,48.93,95.9,48.94,95.89s.65.67,1.41,1.51,2.4.64,3.62-.47Z"/>
+                                <circle class="cls-check-circle" cx="67.53" cy="67.53" r="64.53"/>
+                            </svg>
+                        </span>
+                    </div>
+                    <!--<div class="search-song-queue song-item">-->
+                        <!---->
+                    <!--</div>-->
                 </div>
             </div>
         </div>
@@ -165,7 +196,9 @@
                 playNotDisabled: false,
                 showSongDetails: false,
                 playlistVisible: false,
-                currentSongVisible: false
+                currentSongVisible: false,
+                queueUpVisible: false,
+                queued: false,
             }
         },
 
@@ -216,6 +249,7 @@
                    });
                });
                this.songSearched = true;
+               this.queued = false;
            },
 
            addSong(room, addedSong) {
@@ -251,7 +285,8 @@
                     song_uri: song_uri,
                 }).then(response => {
 
-                })
+                });
+                this.queued = !this.queued;
             },
 
             play() {
@@ -321,12 +356,14 @@
 
             showPlaylist() {
                 this.playlistVisible = !this.playlistVisible;
+                this.queued = false;
             },
             showCurrentSong() {
                 axios.get('/spotify/currently-playing').then(response => {
                     this.currentSong = response.data;
                     this.currentSongVisible = !this.currentSongVisible;
                 });
+                setTimeout(() => this.currentSongVisible = false, 10000)
             }
         }
     }
